94e1b8a34a505c6833d42382acda2f9cd4ef5c0b
Marek <nucl3arfu3l@gmail.com>
2024-02-25 20:53:29 +0100

    Add MIT license

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+"""
+MIT License
+
+Copyright (c) 2024 M6r3k
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
 import tkinter as tk
 from datetime import datetime
 
@@ -13,7 +38,7 @@ def compare_master_and_unit(master_carton, unit_labels):
     current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     # Create the report file with a unique name including the master code value
-    report_file_name = f"{master_code_first_value}_comparison_report_{current_datetime}.txt"
+    report_file_name = f"{master_code_first_value}_report_{current_datetime}.txt"
 
     with open(report_file_name, "w") as report_file:
         report_file.write("Comparison Report\n")
